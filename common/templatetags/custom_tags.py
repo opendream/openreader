c3f59e8dadd80bd682e0598168e0bf4b5952651c
Surakarn Samkaew <tonkla@gmail.com>
2011-07-07 11:34:35 +0700

    Fix display of pending date & time

--- common/templatetags/custom_tags.py
+++ common/templatetags/custom_tags.py
@@ -8,11 +8,16 @@ def site_title():
     return settings.SITE_TITLE
 
 @register.simple_tag()
-def render_pending_time_options():
+def render_pending_time_options(init_time=None):
     tags = []
     for hour in range(24):
         if len(str(hour)) == 1:
             hour = '0%s' % hour
         tags.append('<option value="%s:00">%s:00</option>' % (hour, hour))
         tags.append('<option value="%s:30">%s:30</option>' % (hour, hour))
-    return ''.join(tags)
+    if init_time:
+        time = init_time.strftime('%H:%M')
+        options = ''.join(tags)
+        return options.replace('value="'+ time +'"', 'value="'+ time +'" selected')
+    else:
+        return ''.join(tags)
